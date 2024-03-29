5e0c11b9e5b8acf94e7146e88b7a85d905985797
dnalab <lujie0823@gmail.com>
2024-03-29 21:53:36 +0000

    set up script

--- set_up_armadillo.sh
+++ set_up_armadillo.sh
@@ -4,3 +4,6 @@ mkdir scripts
 mkdir results
 mkdir results/zip
 mkdir cluster
+aws s3 cp --recursive s3://804609861260-bioinformatics-infectious-disease/pipeline/kraken2_db/k2_standard_08gb_20230605 ./ --region="us-gov-west-1"
+aws s3 cp --recursive s3://804609861260-bioinformatics-infectious-disease/pipeline/ARLN/Phoenix/miniconda3 ./ --region="us-gov-west-1"
+aws s3 cp --recursive s3://804609861260-bioinformatics-infectious-disease/pipeline/ARLN/Phoenix/phoenix ./ --region="us-gov-west-1"
