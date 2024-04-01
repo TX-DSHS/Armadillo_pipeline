344401d96dfe9c1051da5ba8df28bab9490d6bcb
jl32587 <lujie0823@gmail.com>
2024-04-01 14:00:46 -0500

    ...

--- set_up_armadillo.sh
+++ set_up_armadillo.sh
@@ -1,3 +1,4 @@
+git clone --branch bfx https://github.com/TX-DSHS/Armadillo_pipeline.git
 mkdir reads
 mkdir reads/zip
 mkdir scripts
@@ -7,3 +8,4 @@ mkdir cluster
 aws s3 cp --recursive s3://804609861260-bioinformatics-infectious-disease/pipeline/kraken2_db/k2_standard_08gb_20230605 ./ --region="us-gov-west-1"
 aws s3 cp --recursive s3://804609861260-bioinformatics-infectious-disease/pipeline/ARLN/Phoenix/miniconda3 ./ --region="us-gov-west-1"
 aws s3 cp --recursive s3://804609861260-bioinformatics-infectious-disease/pipeline/ARLN/Phoenix/phoenix ./ --region="us-gov-west-1"
+# modify aws_bucket.txt file
